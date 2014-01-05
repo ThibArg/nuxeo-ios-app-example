7280a5d88308d954f2020b99319af5e93bfcc2f2
Thibaud Arguillere <thibarg.web@free.fr>
2014-01-04 23:33:12 -0500

    Allow testing with Pods (update env. var and search paths)

---
+++ NuxeoBasicAppTests/NuxeoBasicAppTests.m
@@ -0,0 +1,34 @@
+//
+//  NuxeoBasicAppTests.m
+//  NuxeoBasicApp
+//
+//  Created by Thibaud on 1/4/14.
+//  Copyright (c) 2014 ThibArg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NuxeoBasicAppTests : XCTestCase
+
+@end
+
+@implementation NuxeoBasicAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
